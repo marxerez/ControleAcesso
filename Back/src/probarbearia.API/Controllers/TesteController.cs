20e5f0787f431893d4271fbf7f244de057aa5534
marxerez <marxerez@hotmail.com>
2022-08-18 18:25:14 -0300

    Primeiro commit

---
+++ Back/src/probarbearia.API/Controllers/TesteController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace probarbearia.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesteController : ControllerBase
+    {
+        public TesteController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Retorno via get";
+        }
+        
+        [HttpPost]
+        public string post()
+        {
+            return "Retorno via post";
+        }
+
+        
+        [HttpPut("{id}")]
+        public string put(int id)
+        {
+            return $"Retorno atualiza id ={id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string delete(int id)
+        {
+            return $"Retorno delete id ={id}";
+        }
+    }
+
+
+
+}
